c02b843bc37f1adca88c44f3c66308a5826d47cc
Jorge Romera <romera.gomez.jorge@gmail.com>
2026-03-12 23:57:07 -0300

    refactor club authentication to use ClubUser model and implement onboarding functionality

--- app/Http/Middleware/EnsureUserIsAdmin.php
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,11 +15,11 @@ final class EnsureUserIsAdmin
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  Closure(Request): (Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (! Auth::guard('admin_users')->check()) {
+        if (! Auth::user() instanceof User){
             return response()->json([
                 'message' => 'Unauthenticated.',
             ], 401);
