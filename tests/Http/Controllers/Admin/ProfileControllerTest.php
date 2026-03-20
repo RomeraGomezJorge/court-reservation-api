21d4bdac83ec6554ad28b5c8f4b6e5456e7d4908
Jorge Romera <romera.gomez.jorge@gmail.com>
2026-03-20 19:37:12 -0300

    add ProfileControllerTest; implement test for authenticated admin profile display

---
+++ tests/Http/Controllers/Admin/ProfileControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Http\Controllers\Admin;
+
+use App\Http\Controllers\Admin\ProfileController;
+
+use function Pest\Laravel\get;
+
+it('shows authenticated admin profile', function (): void {
+    $user = actingAsUser();
+
+    get(action([ProfileController::class, 'show']))
+        ->assertOk()
+        ->assertExactJson([
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'roles' => ['super_admin'],
+        ]);
+});
+
