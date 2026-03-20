add2b022047cf88962b5006797c2ea2e042a9819
Jorge Romera <romera.gomez.jorge@gmail.com>
2026-03-20 12:44:03 -0300

    add LoginControllerTest for login functionality and validation cases

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,29 +2,44 @@
 
 declare(strict_types=1);
 
+use App\Models\ClubUser;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Sleep;
 use Illuminate\Support\Str;
+use LaravelIdea\Helper\App\Models\_IH_ClubUser_C;
 use Tests\TestCase;
+
+use function Pest\Laravel\actingAs;
 
 pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
     ->beforeEach(function (): void {
         Str::createRandomStringsNormally();
-        Str::createUuidsNormally();
         Http::preventStrayRequests();
         Process::preventStrayProcesses();
         Sleep::fake();
 
         $this->freezeTime();
     })
-    ->in('Browser', 'Feature', 'Unit');
+    ->in('Http');
 
 expect()->extend('toBeOne', fn () => $this->toBe(1));
 
-function something(): void
+function actingAsClubUser(): ClubUser
 {
-    // ..
+    $clubUser = ClubUser::factory()->create();
+    actingAs($clubUser);
+
+    return $clubUser;
+}
+
+function actingAsUser(): User
+{
+    $user = User::factory()->create();
+    actingAs($user);
+
+    return $user;
 }
